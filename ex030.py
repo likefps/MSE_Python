cd4c81403b60bdb7097fd3bb255a0db8ec41ebf1
likefps <likefps@naver.com>
2020-12-03 08:12:10 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+string='abcd'  # 문자열 abcd
+string.replace('b','B') # 이것을 b --> B 로하면
+print(string)  # 바뀌지 않는다. 이유는 문자열은 변경 불가능한 자료형이기때문
+
+
+# In[ ]:
+
+
+
+
