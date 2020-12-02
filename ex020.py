d81f02d287921bd776aa147f82764e0e57f55976
likefps <likefps@naver.com>
2020-12-03 08:11:07 +0900

    added python file

---
+++ ex020.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#20번문제
+price=48584  # 월 내야하는 금액에
+totalprice=price * 36  # 36개월 만큼이니까 총 금액에 곱해줘야하고
+print(totalprice) # 이것을 출력함
+
+
+# In[ ]:
+
+
+
+
