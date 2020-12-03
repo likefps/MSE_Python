10558c5fb6acae0c4bc28e0f114895f6b94f3f34
likefps <likefps@naver.com>
2020-12-03 10:24:50 +0900

    added python file

---
+++ ex060.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 60번 문제
+nums=[1,2,3,4,5]
+ave=sum(nums)/len(nums)  # sum(nums)는 nums 리스트의 합이고 len(nums)는 리스트의 길이이다. 나눠주면 평균이다.
+ave
+
+
+# In[ ]:
+
+
+
+
