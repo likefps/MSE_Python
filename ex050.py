411d8aa4ef6ffcfb58498a54569b08c7603d375b
likefps <likefps@naver.com>
2020-12-03 10:20:22 +0900

    added python file

---
+++ ex050.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 50번문제
+data='039490   '  # 이건 문자열이다 숫자였으면 ''가 없다.
+data_1=data.rstrip() #data_1 변수에 data를 rstrip처리한걸 넣어준다. rstrip는 오른쪽 공백을 없애준다.
+data_1 # 최종 출력한다.
+
+
+# In[ ]:
+
+
+
+
