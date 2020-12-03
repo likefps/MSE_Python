feb8ee99efc63a6be5b9d22c2ba474c187e609f8
likefps <likefps@naver.com>
2020-12-03 10:18:56 +0900

    added python file

---
+++ ex040.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# 40번문제
+data="     삼성전자     "
+data_1=data.strip()  # 변수 data_1에다가 data를 스트립한걸 넣는다. strip 은 문자열 안에 공백을 없애주는 명령
+print(data_1) # 그걸 최종 출력해라
+
+
+# In[ ]:
+
+
+
+
