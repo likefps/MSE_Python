4151b8cb9fddef08d20789cae703fdc29240eef8
likefps <likefps@naver.com>
2020-12-06 13:57:14 +0900

    added python file

---
+++ ex230.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# 230번 문제
+def my_print(a,b): # 2개의 숫자로 정의된 함수
+    print("왼쪽:",a) # 왼쪽:a 출력
+    print("오른쪽",b)# 오른쪽:b 출력
+
+my_print(b=100,a=200) #근데 a=200대입 b=100 대입 하면 왼 200 오른쪽 100 이렇게 출력된다.
+
+
+# In[ ]:
+
+
+
+
