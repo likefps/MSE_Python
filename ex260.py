40a3ac730f93cd390a8069f3f969f529c6b3d1cd
likefps <likefps@naver.com>
2020-12-06 14:04:55 +0900

    added python file

---
+++ ex260.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#260번 문제
+class OMG : 
+    def print() :  #self(자기자신을 참조하는.. 파이썬 클래스 함수 사용시 self가 필요하다.) 를 써줘야한다.  
+        print("Oh my god")
+mystock=OMG()  
+mystock.print()
+
+
+# In[ ]:
+
+
+
+
