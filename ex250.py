84d436b0c2317cbcd1b9aae4b5b6ab43aa9c9582
likefps <likefps@naver.com>
2020-12-06 14:04:26 +0900

    added python file

---
+++ ex250.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# 250번 문제
+import numpy as np   # 파이썬의 넘파이 기능을 불러오는데 np로 쓰겠다.
+for i in np.arange(0,5.1,0.1): # 리스트에 range 랑똑같은게 넘파이에 arange
+    print(i)  # 출력 
+
+
+# In[ ]:
+
+
+
+
